b751bf795b2a19649a332a5028854f150897b53e
Anita Idemudia <ieghonghon@gmail.com>
2022-10-29 22:47:21 +0100

    dotenv

--- pages/api/submit-rec.ts
+++ pages/api/submit-rec.ts
@@ -1,14 +1,14 @@
 import { NextApiHandler } from "next";
 import { XataClient, getXataClient } from "../../utils/xata.codegen";
 import dotenv from "dotenv"
-// dotenv.config();
+dotenv.config();
 
 
 
 
 const handler: NextApiHandler = async (req, res) => {
-  const xata = await getXataClient();
-  // const xata = new XataClient({apiKey: process.env.XATA_API_KEY});
+  // const xata = await getXataClient();
+  const xata = new XataClient({apiKey: process.env.XATA_API_KEY});
   const { name, email, reason } = req.body;
   await xata.db.viewers.create({
     name,
