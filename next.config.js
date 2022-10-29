c75366fa73491c684d52744890512df29fede8c1
Anita Idemudia <ieghonghon@gmail.com>
2022-10-29 23:27:03 +0100

    dotenv next

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   env: {
-    apiKey: NEXT_PUBLIC_XATA_API_KEY,
+    apiKey: "NEXT_PUBLIC_XATA_API_KEY",
   },
     future: {
       webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
